654bc9623c4740b9990441192de07507339d5c95
Dave Dash <dd+github@davedash.com>
2009-09-17 13:49:43 -0700

    Added tests, docs, etc.

--- alexa/__init__.py
+++ alexa/__init__.py
@@ -1,13 +1,14 @@
 """
-This script downloads the alexa top 1M sites, unzips it, and reads the CSV and returns a list
-of the top N sites.
+This script downloads the alexa top 1M sites, unzips it, and reads the CSV and
+returns a list of the top N sites.
 """
 
-import zipfile, cStringIO
+import zipfile
+import cStringIO
 from urllib import urlopen
 
-
 ALEXA_DATA_URL = 'http://s3.amazonaws.com/alexa-static/top-1m.csv.zip'
+
 
 def alexa_etl():
     """
@@ -16,18 +17,20 @@ def alexa_etl():
         Transforms the data into python via CSV lib
         Loads it to the end user as a python list
     """
-    
+
     f = urlopen(ALEXA_DATA_URL)
     buf = cStringIO.StringIO(f.read())
     zfile = zipfile.ZipFile(buf)
     buf = cStringIO.StringIO(zfile.read('top-1m.csv'))
     for line in buf:
         (rank, domain) = line.split(',')
-        yield (rank, domain.strip())
+        yield (int(rank), domain.strip())
+
 
 def top_list(num=100):
     a = alexa_etl()
     return [a.next() for x in xrange(num)]
-    
+
+
 if __name__ == "__main__":
     print alexa_list()
--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@ from distutils.core import setup
 setup(name  = 'alexa-top-sites',
 version     = '0.1',
 packages    = ['alexa'],
+url = 'http://github.com/davedash/Alexa-Top-Sites',
+author = 'Dave Dash',
+author_email = 'dd+github@davedash.com',
 )
